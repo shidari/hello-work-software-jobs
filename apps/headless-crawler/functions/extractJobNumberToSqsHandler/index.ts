67db8e7dd221e6f1b4202e56aab156416e8b3d84
GitHub Action <action@github.com>
2025-09-28 09:11:33 +0000

    style: auto-format and lint fixes [skip ci]

--- apps/headless-crawler/functions/extractJobNumberToSqsHandler/index.ts
+++ apps/headless-crawler/functions/extractJobNumberToSqsHandler/index.ts
@@ -4,19 +4,19 @@ import { crawlerRunnable } from "../../lib/crawler";
 import { sendJobToQueue } from "./helper";
 
 export const handler: Handler<
-    // biome-ignore lint/complexity/noBannedTypes: <explanation>
-    EventBridgeEvent<"Scheduled Event", {}>,
-    unknown // 型つけるのが面倒くさいので
+  // biome-ignore lint/complexity/noBannedTypes: <explanation>
+  EventBridgeEvent<"Scheduled Event", {}>,
+  unknown // 型つけるのが面倒くさいので
 > = async (_) => {
-    const program = pipe(
-        crawlerRunnable,
-        Effect.tap((jobs) => Effect.forEach(jobs, sendJobToQueue)),
-    );
+  const program = pipe(
+    crawlerRunnable,
+    Effect.tap((jobs) => Effect.forEach(jobs, sendJobToQueue)),
+  );
 
-    const exit = await Effect.runPromiseExit(program);
-    if (Exit.isSuccess(exit)) {
-        console.log("handler succeeded", JSON.stringify(exit.value, null, 2));
-        return exit.value;
-    }
-    throw new Error(`handler failed: ${exit.cause}`);
+  const exit = await Effect.runPromiseExit(program);
+  if (Exit.isSuccess(exit)) {
+    console.log("handler succeeded", JSON.stringify(exit.value, null, 2));
+    return exit.value;
+  }
+  throw new Error(`handler failed: ${exit.cause}`);
 };
