83e827c93ff3f441269c2eba7ab2ed8181113e52
GitHub Action <action@github.com>
2025-10-01 15:23:57 +0000

    style: auto-format and lint fixes [skip ci]

--- apps/job-store-api/test/unit.spec.ts
+++ apps/job-store-api/test/unit.spec.ts
@@ -104,7 +104,7 @@ describe("/api/v1/jobs", () => {
       await waitOnExecutionContext(ctx);
       expect(response.status).toBe(200);
     });
-  })
+  });
   describe("POST 異常系", () => {
     it("with invalid API key should return 401", async () => {
       const request = new Request("http://localhost:8787/api/v1/jobs", {
@@ -134,7 +134,7 @@ describe("/api/v1/jobs", () => {
       await waitOnExecutionContext(ctx);
       expect(response.status).toBe(400);
     });
-  })
+  });
 });
 
 describe("/api/v1/jobs/continue", () => {
