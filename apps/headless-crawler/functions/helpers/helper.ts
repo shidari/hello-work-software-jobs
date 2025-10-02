40c0201123ed3a0dec036e756bbf0ec230861bb1
shidari <s.hidari@icloud.com>
2025-10-02 12:51:28 +0900

    refactor: 未使用のSQS送信ヘルパーと型importを削除
    - sendJobToRawJobDetailExtractorQueue関数と未使用のJobMetadata型importを削除
    - コードの整理・保守性向上のためのリファクタ

--- apps/headless-crawler/functions/helpers/helper.ts
+++ apps/headless-crawler/functions/helpers/helper.ts
@@ -1,5 +1,4 @@
 import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
-import type { JobMetadata } from "@sho/models";
 import { Effect } from "effect";
 import { SendSQSMessageError } from "./error";
 export type Serializable =
@@ -18,27 +17,6 @@ export const sendMessageToQueue = (value: Serializable, url: string) =>
           new SendMessageCommand({
             QueueUrl: url,
             MessageBody: JSON.stringify(value),
-          }),
-        );
-      },
-      catch: (e) =>
-        new SendSQSMessageError({ message: `unexpected error.\n${String(e)}` }),
-    });
-  });
-export const sendJobToRawJobDetailExtractorQueue = (job: JobMetadata) =>
-  Effect.gen(function* () {
-    const sqs = new SQSClient({});
-    const QUEUE_URL = yield* Effect.fromNullable(
-      process.env.RAW_JOB_DETAIL_EXTRACTOR_QUEUE_URL,
-    );
-    return yield* Effect.tryPromise({
-      try: async () => {
-        return await sqs.send(
-          new SendMessageCommand({
-            QueueUrl: QUEUE_URL,
-            MessageBody: JSON.stringify({
-              job,
-            }),
           }),
         );
       },
