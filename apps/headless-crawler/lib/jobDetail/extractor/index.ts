9f037a4ab13512b4ad5e2198a87bec632a089e26
GitHub Action <action@github.com>
2025-10-09 03:55:56 +0000

    style: auto-format and lint fixes [skip ci]

--- apps/headless-crawler/lib/jobDetail/extractor/index.ts
+++ apps/headless-crawler/lib/jobDetail/extractor/index.ts
@@ -4,7 +4,9 @@ import { HelloWorkRawJobDetailHtmlExtractor } from "./context";
 export const buildProgram = (jobNumber: JobNumber) =>
   Effect.gen(function* () {
     const extractor = yield* HelloWorkRawJobDetailHtmlExtractor;
-    yield* Effect.logInfo(`start extracting job detail. jobNumber=${jobNumber}`);
+    yield* Effect.logInfo(
+      `start extracting job detail. jobNumber=${jobNumber}`,
+    );
     const rawHtml = yield* extractor.extractRawHtml(jobNumber);
     return rawHtml;
   });
