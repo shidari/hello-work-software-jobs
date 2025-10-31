95ca1769379bbb00c3c65f82f7aad9e1739df7cd
GitHub Action <action@github.com>
2025-10-31 10:12:13 +0000

    style: auto-format and lint fixes [skip ci]

--- apps/hello-work-job-searcher/src/app/_atom/favorites.ts
+++ apps/hello-work-job-searcher/src/app/_atom/favorites.ts
@@ -5,22 +5,22 @@ export const favoriteJobsAtom = atom<TJobOverview[]>([]);
 
 // favoriteJobsAtomにTJobOverviewをappendし、localStorageにも書き込むwrite-only atom
 export const appendFavoriteJobAtom = atom<null, [TJobOverview], void>(
-    null,
-    (get, set, job) => {
-        const prev = get(favoriteJobsAtom);
-        const next = [...prev, job];
-        set(favoriteJobsAtom, next);
-        localStorage.setItem("favoriteJobs", JSON.stringify(next));
-    },
+  null,
+  (get, set, job) => {
+    const prev = get(favoriteJobsAtom);
+    const next = [...prev, job];
+    set(favoriteJobsAtom, next);
+    localStorage.setItem("favoriteJobs", JSON.stringify(next));
+  },
 );
 
 // favoriteJobsAtomからjobNumberで該当データを削除し、localStorageにも書き込むwrite-only atom
 export const removeFavoriteJobAtom = atom<null, [string], void>(
-    null,
-    (get, set, jobNumber) => {
-        const prev = get(favoriteJobsAtom);
-        const next = prev.filter((job) => job.jobNumber !== jobNumber);
-        set(favoriteJobsAtom, next);
-        localStorage.setItem("favoriteJobs", JSON.stringify(next));
-    },
+  null,
+  (get, set, jobNumber) => {
+    const prev = get(favoriteJobsAtom);
+    const next = prev.filter((job) => job.jobNumber !== jobNumber);
+    set(favoriteJobsAtom, next);
+    localStorage.setItem("favoriteJobs", JSON.stringify(next));
+  },
 );
--- apps/hello-work-job-searcher/src/app/_atom/index.ts
+++ apps/hello-work-job-searcher/src/app/_atom/index.ts
@@ -1,2 +1,2 @@
-export * from "./jobs"
-export * from "./favorites"
+export * from "./jobs";
+export * from "./favorites";
