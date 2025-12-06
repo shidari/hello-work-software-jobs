e8d8c6fa811decb79646afecee8d061527036cb5
shidari <s.hidari@icloud.com>
2025-12-06 12:50:53 +0900

    refactor: jobDetail周り、ブラウザリソース周りのリファクタリング - ちょっと、ブラウザの挙動が変、というか、レイヤーの上書きの仕組みがいまいち分かってないというか

--- apps/headless-crawler/lib/jobDetail/extractor/index.ts
+++ apps/headless-crawler/lib/jobDetail/extractor/index.ts
@@ -1,12 +1,51 @@
 import type { JobNumber } from "@sho/models";
 import { Effect } from "effect";
-import { HelloWorkRawJobDetailHtmlExtractor } from "./context";
-export const buildProgram = (jobNumber: JobNumber) =>
-  Effect.gen(function* () {
-    const extractor = yield* HelloWorkRawJobDetailHtmlExtractor;
-    yield* Effect.logInfo(
-      `start extracting job detail. jobNumber=${jobNumber}`,
-    );
-    const rawHtml = yield* extractor.extractRawHtml(jobNumber);
-    return rawHtml;
-  });
+import { goToJobSearchPage, searchNoThenGotoSingleJobListPage } from "../../core/page/JobSearch/navigations";
+import { validateJobSearchPage } from "../../core/page/JobSearch/validators";
+import { validateJobListPage } from "../../core/page/JobList/validators";
+import { goToSingleJobDetailPage } from "../../core/page/JobList/navigations";
+import { validateJobDetailPage } from "../helpers/validators";
+import { ExtractJobDetailRawHtmlError } from "./error";
+import { format } from "date-fns";
+import { PlaywrightChromiumPageResource } from "../../core/headless-browser";
+
+const i = Symbol();
+type ISODateString = string & { [i]: never };
+
+
+const nowISODateString = (): ISODateString =>
+  format(new Date(), "yyyy-MM-dd") as ISODateString;
+
+export class Extractor extends Effect.Service<Extractor>()("jobDetail/extractor", {
+  effect: Effect.gen(function* () {
+    const pageResource = yield* PlaywrightChromiumPageResource
+    const extractRawHtml = Effect.fn("extractRawHtml")(function* (jobNumber: JobNumber) {
+      const { page } = pageResource;
+      yield* Effect.logInfo("start extracting raw job detail HTML...");
+      yield* Effect.logDebug("go to hello work seach page.");
+      yield* goToJobSearchPage(page);
+      const searchPage = yield* validateJobSearchPage(page);
+      yield* Effect.logDebug(
+        "fill jobNumber then go to hello work seach page.",
+      );
+      yield* searchNoThenGotoSingleJobListPage(searchPage, jobNumber);
+      const jobListPage = yield* validateJobListPage(searchPage);
+      yield* Effect.logDebug("now on job List page.");
+
+      yield* goToSingleJobDetailPage(jobListPage);
+      const jobDetailPage = yield* validateJobDetailPage(jobListPage);
+      const rawHtml = yield* Effect.tryPromise({
+        try: () => jobDetailPage.content(),
+        catch: (error) =>
+          new ExtractJobDetailRawHtmlError({
+            jobNumber,
+            currentUrl: jobDetailPage.url(),
+            reason: `${error instanceof Error ? error.message : String(error)}`,
+          }),
+      });
+      return { rawHtml, fetchedDate: nowISODateString(), jobNumber };
+    })
+    return { extractRawHtml }
+  }),
+  dependencies: [PlaywrightChromiumPageResource.Default]
+}) { }
--- apps/headless-crawler/lib/jobDetail/extractor/playground/index.ts
+++ apps/headless-crawler/lib/jobDetail/extractor/playground/index.ts
@@ -1,18 +1,23 @@
-import { Effect } from "effect";
-import { extractorLive } from "../context";
-import { buildProgram } from "..";
+import { Effect, Logger, LogLevel } from "effect";
 import type { JobNumber } from "@sho/models";
-import { ExtractorConfig } from "../../../service/jobDetail/config";
+import { Extractor } from "..";
+import { PlaywrightBrowserConfig, PlaywrightChromiumBrowseResource } from "../../../core/headless-browser";
 
 async function main() {
-  const jobNumber = "01010-24871951" as JobNumber;
-  const program = buildProgram(jobNumber);
-  // いまいち書き方がわかってない
+  const jobNumber = "01010-46772851" as JobNumber;
+  const program = Effect.gen(function* () {
+    const extractor = yield* Extractor
+    const jobInfo = yield* extractor.extractRawHtml(jobNumber);
+    return jobInfo;
+  })
   const runnable = program
-    .pipe(Effect.provide(extractorLive))
-    .pipe(Effect.provide(ExtractorConfig.Default))
-    // これ、使い方わかってない、なんとなく、playwrightのclose処理に必要なRequirementなのかなと
-    .pipe(Effect.scoped);
+    .pipe(Effect.provide(
+      Extractor.Default
+    ))
+    .pipe(Effect.provide(PlaywrightChromiumBrowseResource.DefaultWithoutDependencies))
+    .pipe(Effect.provideService(PlaywrightBrowserConfig, PlaywrightBrowserConfig.dev))
+    .pipe(Effect.scoped)
+    .pipe(Logger.withMinimumLogLevel(LogLevel.Debug))
   Effect.runPromise(runnable).then((jobInfo) =>
     console.dir({ ...jobInfo }, { depth: null }),
   );
